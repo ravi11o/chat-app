933a10c3c41678b2392be61a38a333d44ed047ad
ravi11o <ravi.suraj110@gmail.com>
2019-01-28 22:43:41 +0530

    add backend for chat-app in socketio

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+// var io = require('socket.io')(http);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
