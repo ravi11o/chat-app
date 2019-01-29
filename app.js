3b7e1988486a1c8830e7820c069f4a70ef8db137
ravi11o <ravi.suraj110@gmail.com>
2019-01-29 17:50:16 +0530

    resolve socket error

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
-var app = express();
-// var io = require('socket.io')(http);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,23 +20,60 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// var chat = io.of('/chat');
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+	socket.emit('news', 'Welcome to chat app');
+	socket.on('chat message', function(msg){
+		console.log(msg);
+	  io.emit('chat message', msg);
+	});
+
+	socket.on('disconnect', () => {
+		console.log('user disconnected')
+	})
+});
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
 
 module.exports = app;
+
+// var express = require('express');
+// var app = express();
+// var http = require('http').createServer(app);
+// var cookieParser = require('cookie-parser');
+// var io = require('socket.io')(http);
+// var port = process.env.PORT || 3000;
+// var path = require('path');
+
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('/', function(req, res){
+//   res.render('index');
+// });
+
+// io.on('connection', function(socket){
+// 	console.log('connected');
+//   socket.on('chat message', function(msg){
+//     io.emit('chat message', msg);
+//   });
+// });
+
+// http.listen(port, function(){
+//   console.log('listening on *:' + port);
+// });
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.send('Hello Express');
+});
+
+router.get('/chat', function(req, res) {
+  res.render('index');
 });
 
 module.exports = router;
